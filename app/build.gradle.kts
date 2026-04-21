c6d3e23c3f24d36fc853adbb9161caffa070c453
Your Name <qianxuntudou@gmail.com>
2026-04-21 23:57:39 +0800

    v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.tutu.myblbl"
         minSdk = 23
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.1.0"
+        versionCode = 11
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
